ab3da329829d4eaf1b83321e463608e529abc74e
André Pimentel <anfarp@avanade.com>
2019-08-05 12:40:02 -0300

    Criação da estrutura do localization

---
+++ DaniBot.Service/Localization/Localization.MainDialog.cs
@@ -0,0 +1,11 @@
+﻿namespace Avanade.HackathonAzul.DaniBot.Localization
+{
+	public partial struct Localization
+	{
+		public MainMessages MainDialog { get; set; }
+		public struct MainMessages
+		{
+			public string Welcome { get; set; }
+		}
+	}
+}
