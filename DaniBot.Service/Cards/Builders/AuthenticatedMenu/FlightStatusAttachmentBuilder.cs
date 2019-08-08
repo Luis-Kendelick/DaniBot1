e2e8c6fe266e158ec9637c410010a7093d08473f
Ariel Medeiros <a.a.medeiros.cintra@avanade.com>
2019-08-07 23:52:02 -0300

    Ajustes  card de flights e authenticates

--- DaniBot.Service/Cards/Builders/AuthenticatedMenu/FlightStatusAttachmentBuilder.cs
+++ DaniBot.Service/Cards/Builders/AuthenticatedMenu/FlightStatusAttachmentBuilder.cs
@@ -7,138 +7,139 @@ using System.Linq;
 
 namespace Avanade.HackathonAzul.DaniBot.Cards.Builders.AuthenticatedMenu
 {
-    public class FlightStatusAttachmentBuilder : IAuthenticatedMenuAttachmentBuilder
-    {
-        private IList<FlightInformationModel> _FlightInformationModels;
+	public class FlightStatusAttachmentBuilder : IAuthenticatedMenuAttachmentBuilder
+	{
+		private IList<FlightInformationModel> _FlightInformationModels;
 
-        public FlightStatusAttachmentBuilder(IList<FlightInformationModel> flightInformationModels)
-        {
-            _FlightInformationModels = flightInformationModels;
-        }
+		public FlightStatusAttachmentBuilder(IList<FlightInformationModel> flightInformationModels)
+		{
+			_FlightInformationModels = flightInformationModels;
+		}
 
-        public Attachment Build()
-        {
-            return new Attachment
-            {
-                ContentType = AdaptiveCard.ContentType,
-                Content = new AdaptiveCard("1.0")
-                {
-                    Type = AdaptiveCard.TypeName,
-                    Body = _FlightInformationModels
-                        .Select(info => new AdaptiveContainer
-                        {
-                            Items = new List<AdaptiveElement>
-                            {
-                                new AdaptiveColumnSet
-                                {
-                                    Columns = new List<AdaptiveColumn>
-                                    {
-                                        new AdaptiveColumn
-                                        {
-                                            Items = new List<AdaptiveElement>
-                                            {
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.DepartureDate.ToShortDateString(),
-                                                    Color = AdaptiveTextColor.Attention,
-                                                    Weight = AdaptiveTextWeight.Bolder,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Left
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.DepartureDate.ToShortTimeString(),
-                                                    Color = AdaptiveTextColor.Attention,
-                                                    Weight = AdaptiveTextWeight.Bolder,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Left
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.DepartureAirport,
-                                                    Size = AdaptiveTextSize.Small,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Left
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.DepartureAirport,
-                                                    Color = AdaptiveTextColor.Accent,
-                                                    Size = AdaptiveTextSize.Large,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Left
-                                                }
-                                            }
-                                        },
-                                        new AdaptiveColumn
-                                        {
-                                            Items = new List<AdaptiveElement>
-                                            {
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = "Localizador",
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Center
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.Number,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Center
-                                                },
-                                                new AdaptiveImage
-                                                {
-                                                    Url = new Uri("http://adaptivecards.io/content/airplane.png"),
-                                                    Size = AdaptiveImageSize.Small,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Center,
-                                                    Spacing = AdaptiveSpacing.Medium
-                                                }
-                                            }
-                                        },
-                                        new AdaptiveColumn
-                                        {
-                                            Items = new List<AdaptiveElement>
-                                            {
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.ArrivalDate.ToShortDateString(),
-                                                    Color = AdaptiveTextColor.Attention,
-                                                    Weight = AdaptiveTextWeight.Bolder,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Right
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.ArrivalDate.ToShortTimeString(),
-                                                    Color = AdaptiveTextColor.Attention,
-                                                    Weight = AdaptiveTextWeight.Bolder,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Right
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.ArrivalAirport,
-                                                    Size = AdaptiveTextSize.Small,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Right
-                                                },
-                                                new AdaptiveTextBlock
-                                                {
-                                                    Text = info.ArrivalAirport,
-                                                    Color = AdaptiveTextColor.Accent,
-                                                    Size = AdaptiveTextSize.Large,
-                                                    HorizontalAlignment = AdaptiveHorizontalAlignment.Right
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            },
-                            SelectAction = new AdaptiveSubmitAction
-                            {
-                                Id = info.Number
-                            }
-                        })
-                        .Cast<AdaptiveElement>()
-                        .ToList()
-                    ,
-                    Actions = new List<AdaptiveAction>
-                    {
+		public Attachment Build()
+		{
+			return new Attachment
+			{
+				ContentType = AdaptiveCard.ContentType,
+				Content = new AdaptiveCard("1.0")
+				{
+					Type = AdaptiveCard.TypeName,
+					Body = _FlightInformationModels
+						.Select(info => new AdaptiveContainer
+						{
+							Items = new List<AdaptiveElement>
+							{
+								new AdaptiveColumnSet
+								{
+									Columns = new List<AdaptiveColumn>
+									{
+										new AdaptiveColumn
+										{
+											Items = new List<AdaptiveElement>
+											{
+												new AdaptiveTextBlock
+												{
+													Text = info.DepartureDate.ToShortDateString(),
+													Color = AdaptiveTextColor.Attention,
+													Weight = AdaptiveTextWeight.Bolder,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Left
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.DepartureDate.ToShortTimeString(),
+													Color = AdaptiveTextColor.Attention,
+													Weight = AdaptiveTextWeight.Bolder,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Left
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.DepartureAirport,
+													Size = AdaptiveTextSize.Small,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Left
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.DepartureAirport,
+													Color = AdaptiveTextColor.Accent,
+													Size = AdaptiveTextSize.Large,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Left
+												}
+											}
+										},
+										new AdaptiveColumn
+										{
+											Items = new List<AdaptiveElement>
+											{
+												new AdaptiveTextBlock
+												{
+													Text = "Localizador",
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Center
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.Number,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Center
+												},
+												new AdaptiveImage
+												{
+													Url = new Uri("http://adaptivecards.io/content/airplane.png"),
+													Size = AdaptiveImageSize.Small,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Center,
+													Spacing = AdaptiveSpacing.Medium
+												}
+											}
+										},
+										new AdaptiveColumn
+										{
+											Items = new List<AdaptiveElement>
+											{
+												new AdaptiveTextBlock
+												{
+													Text = info.ArrivalDate.ToShortDateString(),
+													Color = AdaptiveTextColor.Attention,
+													Weight = AdaptiveTextWeight.Bolder,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Right
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.ArrivalDate.ToShortTimeString(),
+													Color = AdaptiveTextColor.Attention,
+													Weight = AdaptiveTextWeight.Bolder,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Right
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.ArrivalAirport,
+													Size = AdaptiveTextSize.Small,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Right
+												},
+												new AdaptiveTextBlock
+												{
+													Text = info.ArrivalAirport,
+													Color = AdaptiveTextColor.Accent,
+													Size = AdaptiveTextSize.Large,
+													HorizontalAlignment = AdaptiveHorizontalAlignment.Right
+												}
+											}
+										}
+									}
+								}
+							},
+							SelectAction = new AdaptiveSubmitAction
+							{
+								Title = info.Number,
+								Data = info
+							}
+						})
+						.Cast<AdaptiveElement>()
+						.ToList()
+					,
+					Actions = new List<AdaptiveAction>
+					{
 
-                    }
-                }
-            };
-        }
-    }
+					}
+				}
+			};
+		}
+	}
 }
--- DaniBot.Service/Dialogs/AuthenticatedMenuDialog.cs
+++ DaniBot.Service/Dialogs/AuthenticatedMenuDialog.cs
@@ -12,13 +12,87 @@ namespace Avanade.Azul.DaniBot.Dialogs
 {
     public class AuthenticatedMenuDialog : ComponentDialog
 	{
+		#region MOQ
+		private FlightStatusAttachmentBuilder _FlightStatusMoq = new FlightStatusAttachmentBuilder(
+				new List<FlightInformationModel>
+				{
+					new FlightInformationModel
+					{
+						DepartureAirport = "SAO",
+						ArrivalAirport = "REC",
+						DepartureCity = "Sao Paulo",
+						ArrivalCity = "Recife",
+						DepartureDate = new DateTime(2019, 8, 7, 22, 30, 0),
+						ArrivalDate = new DateTime(2019, 8, 8, 3, 45, 0),
+						Number = "FH4JJ1",
+						Status = FlightStatus.OnTime
+	},
+					new FlightInformationModel
+					{
+						DepartureAirport = "REC",
+						ArrivalAirport = "SAO",
+						DepartureCity = "Recife",
+						ArrivalCity = "Sao Paulo",
+						DepartureDate = new DateTime(2019, 9, 7, 21, 30, 0),
+						ArrivalDate = new DateTime(2019, 9, 8, 2, 30, 0),
+						Number = "FH4JJ2",
+						Status = FlightStatus.OnTime
+},
+					new FlightInformationModel
+					{
+						DepartureAirport = "SAO",
+						ArrivalAirport = "REC",
+						DepartureCity = "Sao Paulo",
+						ArrivalCity = "Recife",
+						DepartureDate = new DateTime(2019, 10, 7, 20, 30, 0),
+						ArrivalDate = new DateTime(2019, 10, 8, 1, 15, 0),
+						Number = "FH4JJ3",
+						Status = FlightStatus.OnTime
+					}
+				});
+
+		private TudoAzulAttachmentBuilder _TudoAzulMoq = new TudoAzulAttachmentBuilder(
+				new TodoAzulSummaryModel
+				{
+					CardNumber = "1234 5678 9012 3456",
+					Balance = 4500
+				});
+
+		private PromoCardAttachmentBuilder _Promos = new PromoCardAttachmentBuilder(
+				new List<PromoInformationModel>
+				{
+					new PromoInformationModel
+					{
+						Title = "PROMO 1",
+						Content = "VEJA BEM COMO ESSA PROMOÇÃO É INTERESSANTE",
+						HyperLinkUrl = "https://www.google.com/search?q=promo%C3%A7%C3%B5es%20azul",
+						ImageUrl = "https://cdnv2.moovin.com.br/lojasplasutil/imagens/produtos/original/produto-teste-d6363200ff6d085db9adf4f244a841d3.png"
+					},
+					new PromoInformationModel
+					{
+						Title = "PROMO 2",
+						Content = "VEJA BEM COMO ESSA PROMOÇÃO É MUITO MAIS INTERESSANTE",
+						HyperLinkUrl = "https://www.google.com/search?q=promo%C3%A7%C3%B5es%20azul",
+						ImageUrl = "https://cdnv2.moovin.com.br/lojasplasutil/imagens/produtos/original/produto-teste-d6363200ff6d085db9adf4f244a841d3.png"
+					},
+					new PromoInformationModel
+					{
+						Title = "PROMO 3",
+						Content = "VEJA BEM COMO ESSA PROMOÇÃO É AINDA MAIS INTERESSANTE",
+						HyperLinkUrl = "https://www.google.com/search?q=promo%C3%A7%C3%B5es%20azul",
+						ImageUrl = "https://cdnv2.moovin.com.br/lojasplasutil/imagens/produtos/original/produto-teste-d6363200ff6d085db9adf4f244a841d3.png"
+					}
+				});
+		#endregion
+
 		public AuthenticatedMenuDialog()
 			: base(nameof(AuthenticatedMenuDialog))
 		{
 			AddDialog(new TextPrompt(nameof(TextPrompt)));
 			AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
 			{
-				IntroStep
+				IntroStep,
+				ActionStep
 			}));
 
 			InitialDialogId = nameof(WaterfallDialog);
@@ -28,70 +102,19 @@ namespace Avanade.Azul.DaniBot.Dialogs
 		{
             var authenticatedMenuAttachmentBuilders = new List<IAuthenticatedMenuAttachmentBuilder>();
 
-            authenticatedMenuAttachmentBuilders.Add(new FlightStatusAttachmentBuilder(
-                new List<FlightInformationModel>
-                {
-                    new FlightInformationModel
-                    {
-                        DepartureAirport = "SAO",
-                        ArrivalAirport = "REC",
-                        DepartureCity = "Sao Paulo",
-                        ArrivalCity = "Recife",
-                        DepartureDate = new DateTime(2019, 8, 7, 22, 30, 0),
-                        ArrivalDate = new DateTime(2019, 8, 8, 3, 45, 0),
-                        Number = "FH4JJ1",
-                        Status = FlightStatus.OnTime
-                    },
-                    new FlightInformationModel
-                    {
-                        DepartureAirport = "REC",
-                        ArrivalAirport = "SAO",
-                        DepartureCity = "Recife",
-                        ArrivalCity = "Sao Paulo",
-                        DepartureDate = new DateTime(2019, 9, 7, 21, 30, 0),
-                        ArrivalDate = new DateTime(2019, 9, 8, 2, 30, 0),
-                        Number = "FH4JJ2",
-                        Status = FlightStatus.OnTime
-                    },
-                    new FlightInformationModel
-                    {
-                        DepartureAirport = "SAO",
-                        ArrivalAirport = "REC",
-                        DepartureCity = "Sao Paulo",
-                        ArrivalCity = "Recife",
-                        DepartureDate = new DateTime(2019, 10, 7, 20, 30, 0),
-                        ArrivalDate = new DateTime(2019, 10, 8, 1, 15, 0),
-                        Number = "FH4JJ3",
-                        Status = FlightStatus.OnTime
-                    }
-                }));
+            authenticatedMenuAttachmentBuilders.Add(_FlightStatusMoq);
 
-            authenticatedMenuAttachmentBuilders.Add(new PromoCardAttachmentBuilder(
-                new List<PromoInformationModel>
-                {
-                    new PromoInformationModel
-                    {
-                        
-                    },
-                    new PromoInformationModel
-                    {
+            authenticatedMenuAttachmentBuilders.Add(_Promos);
 
-                    },
-                    new PromoInformationModel
-                    {
-
-                    }
-                }));
-
-            authenticatedMenuAttachmentBuilders.Add(new TudoAzulAttachmentBuilder(
-                new TodoAzulSummaryModel
-                {
-                    CardNumber = "1234 5678 9012 3456",
-                    Balance = 4500
-                }));
+            authenticatedMenuAttachmentBuilders.Add(_TudoAzulMoq);
 
             await stepContext.Context.SendActivityAsyncFromAdaptiveCard(CardFactory.CreateAuthenticatedMenuCardBuilder(authenticatedMenuAttachmentBuilders), cancellationToken, isCarousel: true);
 			return new DialogTurnResult(DialogTurnStatus.Waiting);
+		}
+
+		private async Task<DialogTurnResult> ActionStep(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+		{
+			return await stepContext.EndDialogAsync(cancellationToken);
 		}
 	}
 }
